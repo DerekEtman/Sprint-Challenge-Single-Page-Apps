e8729031b69f22361a88beaf1a9232663ca301eb
unknown <dereketman@gmail.com>
2019-08-23 13:02:34 -0700

    created episodes components

---
+++ src/components/EpisodesList.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import EpisodeCard from "./EpisodeCard.js";
+
+export default function EpisodeList(){
+
+};s
