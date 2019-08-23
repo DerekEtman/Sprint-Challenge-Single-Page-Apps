4475f89eb7bc4f60bbde56f31115cddb81383d10
unknown <dereketman@gmail.com>
2019-08-23 11:50:48 -0700

    Character cards works

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(data) {
+  const { image, name, status, species, gender, location, id } = data;
+  // const id = props.match.params.id;
+
+  console.log("data.character: ", data.character);
+  console.log("Name: ", data.character.name);
+
+  return (
+    <div className="characterCard" key={data.character.id}>
+      <img src={data.character.image} />
+      <h2>{data.character.name}</h2>
+      <p>{data.character.species}</p>
+      <p>{data.character.status}</p>
+      <p>{data.character.gender}</p>
+      <p>{data.character.location.name}</p>
+    </div>
+  
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,41 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import CharacterCard from "./CharacterCard.js";
 
-export default function CharacterList(props) {
+export default function CharacterList() {
 
   const [ char, setChar ] = useState([]);
-
+  console.log("char state: ", char);
+  
   // TODO: Add API Request here - must run in `useEffect`
   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   
   // `https://rickandmortyapi.com/api/character/`
-
+  
   useEffect(() => {
     const getChars = () =>{
       axios
-        .get('https://rickandmortyapi.com/api/character/')
-        .then( resp => {
-          setChar(resp.data);
-          console.log(resp);
-        })
-        .catch( error =>{
-          console.log("Axios Err: ", error);
-        });
+      .get('https://rickandmortyapi.com/api/character/')
+      .then( resp => {
+        setChar(resp.data.results);
+      })
+      .catch( error =>{
+        console.log("Axios Err: ", error);
+      });
     }
     getChars();
   }, []);
-
+  
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
-      { char.map(character => (
-        <CharDetails key={character.id} character={character} />
-      ))}
+      { char.map(data => (
+        <CharacterCard key={data.id} character={data} />
+        ))}
     </section>
   );
-}
-
-function CharDetails({ character }) {
-
-}
+};
